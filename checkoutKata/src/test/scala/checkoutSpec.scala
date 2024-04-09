a741d3ceb5b3b8c13922e36e256b16dab71dd8db
Ben McAllister <ben.mcallister@itv.com>
2024-04-09 14:45:45 +0100

    comments

--- checkoutKata/src/test/scala/checkoutSpec.scala
+++ checkoutKata/src/test/scala/checkoutSpec.scala
@@ -1,22 +1,25 @@
 import org.scalatest.funsuite.AnyFunSuite
-import checkoutKataCode.Checkout
-import checkoutKataCode.PricingRule
-
+import checkoutKataCode._
 class CheckoutSpec extends AnyFunSuite {
-  test("total price calculation") {
-    val pricingRules = Map(
-      'a' -> PricingRule(50, 3, 130),
-      'b' -> PricingRule(30, 2, 45),
-      'c' -> PricingRule(20, 1, 20),
-      'd' -> PricingRule(15, 1, 15)
-    )
-    val checkout = new Checkout(pricingRules)
+  // these vals could be reused for other potential tests
+  // so may as well put them in scope to all tests, not defined inside one.
+  val pricingRules = Map(
+    'a' -> PricingRule(50, 3, 130),
+    'b' -> PricingRule(30, 2, 45),
+    'c' -> PricingRule(20, 1, 20),
+    'd' -> PricingRule(15, 1, 15)
+  )
+  val checkout = new Checkout(pricingRules)
+  val items = List('a', 'a', 'a', 'b', 'b', 'c', 'd', 'x')
 
-    val items = List('a', 'a', 'a', 'b', 'b', 'c', 'd', 'x')
-    val totalPrice = checkout.calculateTotalPrice(items)
+
+  // you could now write a test, checking that the countItemsToMap method is doing what it should
+  // again this is pretty trivial stuff, but good to get in the habit of having methods that are easy to test
+  // (they only do one thing)
+  test("total price calculation") {
+    val countItems: Map[Char, Int] = checkout.countItemsToMap(items)
+    val totalPrice: Int = checkout.calculateTotalPrice(countItems)
 
     assert(totalPrice == 130 + 45 + 20 + 15)
   }
 }
-
-
